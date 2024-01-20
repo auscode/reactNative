e97a53a954e085917fe18eb44b872d7a9b3f40e2
auscode <harshittext@gmail.com>
2024-01-20 21:23:19 +0530

    styled cards in 02

--- Styler02/components/FancyCard.tsx
+++ Styler02/components/FancyCard.tsx
@@ -13,8 +13,8 @@ export default function FancyCard() {
           style={styles.cardImage}
         />
         <View style={styles.cardBody}>
-          <Text style={styles.cardTitle}> Jai Ganesha</Text>
-          <Text style={styles.cardLabel}> Ganpati</Text>
+          <Text style={styles.cardTitle}>Jai Ganesha</Text>
+          <Text style={styles.cardLabel}>Ganpati</Text>
           <Text style={styles.cardDesc}>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab
             voluptatibus, magni esse at vero quas!
@@ -30,16 +30,56 @@ const styles = StyleSheet.create({
   headingText: {
     fontSize: 24,
     fontWeight: 'bold',
-    paddingHorizontal: 10,
+    paddingHorizontal: 8,
     color: 'white',
   },
-  card: {},
-  cardElevated: {},
-  cardImage: {
-    height: 200,
+  card: {
+    color: 'white',
+    height: 360,
+    width: 400,
+    borderRadius: 6,
+    marginVertical: 12,
+    marginHorizontal: 12,
   },
-  cardBody: {},
-  cardTitle: {},
-  cardLabel: {},
-  cardFooter: {},
+  cardElevated: {
+    backgroundColor: 'white',
+    elevation: 3,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+  },
+  cardImage: {
+    marginBottom: 8,
+    height: 200,
+    borderTopLeftRadius: 6,
+    borderTopRightRadius: 6,
+  },
+  cardBody: {
+    flex: 1,
+    flexGrow: 1,
+    paddingHorizontal: 12,
+  },
+  cardTitle: {
+    color: '#000000',
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 6,
+  },
+  cardLabel: {
+    color: '#000000',
+    fontSize: 14,
+    marginBottom: 6,
+  },
+  cardDesc: {
+    color: '#242B2E',
+    fontSize: 12,
+    marginBottom: 12,
+    marginTop: 6,
+    flexShrink: 1,
+  },
+  cardFooter: {
+    color: '#000000',
+    fontSize: 12,
+  },
 });
