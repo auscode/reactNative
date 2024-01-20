35a6f39639d899b21ef1d67fe55c7a73ed498242
auscode <harshittext@gmail.com>
2024-01-20 20:27:21 +0530

    project 2 started Created Horizontal Scroll

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Awesome is at [here](https://github.com/auscode/React-Native)
+
+For proejct initiation
+`npx react-native@latest init Styler02`
---
+++ Styler02/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {SafeAreaView, ScrollView} from 'react-native';
+import FlatCards from './components/FlatCards';
+import ElevatedCards from './components/ElevatedCards';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <FlatCards />
+        <ElevatedCards />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
