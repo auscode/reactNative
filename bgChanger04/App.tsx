b624dad2c281d3ec4ddaeeb05e7e48317b0d69e5
auscode <harshittext@gmail.com>
2024-01-22 09:56:59 +0530

    Project 4 bgchanger04 started

---
+++ bgChanger04/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+function App(): React.JSX.Element {
+  return <Text>App</Text>;
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
