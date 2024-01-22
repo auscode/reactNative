1d12b7547631e3e5a11d0ab4d6ba11d6a068d33f
auscode <harshittext@gmail.com>
2024-01-22 10:57:46 +0530

    Project 04 completed

--- bgChanger04/App.tsx
+++ bgChanger04/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
+  TouchableOpacity,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
 function App(): React.JSX.Element {
-  return <Text>App</Text>;
+  const [randomBackground, setrandomBackground] = useState('#FFFFFF');
+
+  const generateColor = () => {
+    const hexRange = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += hexRange[Math.floor(Math.random() * 16)];
+    }
+    setrandomBackground(color);
+  };
+  return (
+    <>
+      <StatusBar backgroundColor={randomBackground} />
+      <View style={[styles.container, {backgroundColor: randomBackground}]}>
+        <TouchableOpacity onPress={generateColor}>
+          <View style={styles.actionBtn}>
+            <Text style={styles.actionBtnTxt}>Press Me</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  actionBtn: {
+    borderRadius: 12,
+    backgroundColor: '#6a1b4d',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+  },
+  actionBtnTxt: {
+    fontSize: 24,
+    color: '#ffffff',
+    textTransform: 'uppercase',
+  },
+});
 
 export default App;
