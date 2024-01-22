dc8a005bfceb54a90d301dd78f5046eea6618749
auscode <harshittext@gmail.com>
2024-01-22 11:20:40 +0530

    project 05 rollDice started

---
+++ rollDice05/src/App.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+// import DiceOne from '../assets/One.png';
+
+function App(): React.JSX.Element {
+  return (
+    <View>
+      <Text>core</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF2F2',
+  },
+  diceContainer: {
+    margin: 12,
+  },
+  diceImage: {
+    width: 200,
+    height: 200,
+  },
+  rollDiceBtnText: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: '#E5E0FF',
+    fontSize: 16,
+    color: '#8EA7E9',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+  },
+});
+
+export default App;
