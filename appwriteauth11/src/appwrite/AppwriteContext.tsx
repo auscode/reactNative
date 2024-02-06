4f0487e688e1aee27069e8181e8d5edd43f5332f
auscode <harshittext@gmail.com>
2024-02-06 12:11:00 +0530

    create context in 11 appwrite

---
+++ appwriteauth11/src/appwrite/AppwriteContext.tsx
@@ -0,0 +1,31 @@
+import React, {FC, PropsWithChildren, createContext, useState} from 'react';
+
+import Appwrite from './service';
+
+type AppContextType = {
+  appwite: Appwrite;
+  isLoggedIn: boolean;
+  setIsLoggedIn: (isLoggedIn: boolean) => void;
+};
+
+export const AppwriteContext = createContext<AppContextType>({
+  appwite: new Appwrite(),
+  isLoggedIn: false,
+  setIsLoggedIn: () => {},
+});
+
+export const AppwriteProvider: FC<PropsWithChildren> = ({children}) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const defaultValue = {
+    appwite: new Appwrite(),
+    isLoggedIn,
+    setIsLoggedIn,
+  };
+
+  return (
+    <AppwriteContext.Provider value={defaultValue}>
+      {children}
+    </AppwriteContext.Provider>
+  );
+};
+export default AppwriteContext;
